cd0a8e89ce81ab0f9b950551f716cba9bcce4792
Doug Binder <douglasjbinder@gmail.com>
2021-12-21 22:55:22 -0600

    test: proof/<name> missing input error message

--- src/main.rs
+++ src/main.rs
@@ -285,4 +285,54 @@ mod test {
             .dispatch();
         assert_eq!(response.status(), Status::BadRequest);
     }
+    #[test]
+    fn test_missing_proof_arg() {
+        let rocket_instance = rocket();
+        let client = Client::tracked(rocket_instance).expect("valid rocket instance");
+
+        let prover = ProverConfig {
+            name: String::from("test"),
+            version: String::from("0.0.1"),
+            path_to_r1cs: String::from("https://unpkg.com/@darkforest_eth/snarks@6.6.6/move.r1cs"),
+            path_to_wasm: String::from("https://unpkg.com/@darkforest_eth/snarks@6.6.6/move.wasm"),
+            path_to_zkey: String::from("https://unpkg.com/@darkforest_eth/snarks@6.6.6/move.zkey"),
+            builder_params: vec![
+                String::from("x1"),
+                String::from("y1"),
+                String::from("x2"),
+                String::from("y2"),
+                String::from("r"),
+                String::from("distMax"),
+                String::from("PLANETHASH_KEY"),
+                String::from("SPACETYPE_KEY"),
+                String::from("SCALE"),
+                String::from("xMirror"),
+                String::from("yMirror"),
+            ],
+        };
+        let response = client.post("/v1/prover").json(&prover).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        let mut proof_request: HashMap<String, u64> = HashMap::new();
+
+        proof_request.insert(String::from("x1"), 100);
+        proof_request.insert(String::from("y1"), 100);
+        proof_request.insert(String::from("y2"), 120);
+        proof_request.insert(String::from("r"), 8000);
+        proof_request.insert(String::from("distMax"), 0);
+        proof_request.insert(String::from("PLANETHASH_KEY"), 1729);
+        proof_request.insert(String::from("SPACETYPE_KEY"), 1730);
+        proof_request.insert(String::from("xMirror"), false as u64);
+        proof_request.insert(String::from("SCALE"), 16384);
+        proof_request.insert(String::from("yMirror"), false as u64);
+
+        let response = client
+            .post("/v1/prove/test")
+            .json(&proof_request)
+            .dispatch();
+        assert_eq!(response.status(), Status::BadRequest);
+        assert_eq!(
+            response.into_string().unwrap(),
+            String::from("x2 is missing from your inputs")
+        )
+    }
 }
