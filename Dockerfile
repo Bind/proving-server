38da6718f8e8d0420368a919a0a9695a7c3fa88f
Doug Binder <douglasjbinder@gmail.com>
2022-01-02 15:08:17 -0600

    chore: cors

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,18 @@
-FROM rust:1.57.0
-
-ENV ROCKET_ADDRESS=0.0.0.0
-ENV ROCKET_PORT=6666
+FROM rust:1.57-slim
 
 WORKDIR /app
 COPY . .
 
-RUN rustup default nightly
-RUN cargo build
+RUN set -ex; \ 
+  apt-get update; \
+  apt-get install -y --no-install-recommends \
+  pkg-config \
+  libssl-dev \
+  sqlite3 libsqlite3-dev
+
+ENV ROCKET_ADDRESS=0.0.0.0
+ENV ROCKET_PORT=6666
+
+RUN cargo build --release
 
 CMD ["cargo", "run"]
--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -8,14 +8,44 @@ mod types;
 mod utils;
 mod worker;
 use std::sync::mpsc;
-
 extern crate dotenv;
-
+extern crate rocket_cors;
 #[macro_use]
 extern crate rocket;
 
-#[launch]
-fn rocket() -> _ {
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
+#[rocket::main]
+async fn main() -> Result<(), crate::errors::ProvingServerError> {
+    rocket().attach(CORS).launch().await.unwrap();
+    Ok(())
+}
+
+fn rocket() -> rocket::Rocket<rocket::Build> {
     utils::load_environment_variables();
     let (tx, rx) = mpsc::sync_channel(1);
     let config = utils::init_config();
@@ -43,7 +73,6 @@ fn rocket() -> _ {
             ],
         )
 }
-
 /**
  *
  *
